4a81790990b03be38e775aea3a20b9f04f1cb641
niccololazzara <67160096+niccololazzara@users.noreply.github.com>
2021-04-20 14:09:38 -0400

    flexible sparql, API occupation dict, boolean occupation addon

--- feature_gather.py
+++ feature_gather.py
@@ -1,18 +1,55 @@
+#import os
+#os.chdir("C://brain//rutgers//564//wikiproject")
+
 from scrapes import *
 from sparql_to_dataframe import *
 
-for i in ppl.QID:
-    p['ids'] = i
-    data = S.get(url=URL, params=p)
-    jdata = json.loads(data.text)
-    occl = []
-    for j in jdata['entities'][p['ids']]['claims']['P106']:
-        qocc = j['mainsnak']['datavalue']['value']['id']
-        occl.append(qocc)
-        occ_list.append(qocc)
-    po_dict[i] = occl
+import pandas as pd
+import requests, json
 
-occu = list(set(occ_list))
-occs = set(occ_list)
-po_dict
-list(po_dict.values())
+def occ_mask(table):
+	po_dict = {}
+	occ_list = []
+	S = requests.Session()
+	p = {
+		"action": "wbgetentities",
+		"ids": "0",
+		"languages": "en",
+		"props": "claims",
+		"format": "json"
+	}
+	for i in table.QID:
+		p['ids'] = i
+		data = S.get(url="https://www.wikidata.org/w/api.php", params=p)
+		jdata = json.loads(data.text)
+		occl = []
+		for j in jdata['entities'][p['ids']]['claims']['P106']:
+			qocc = j['mainsnak']['datavalue']['value']['id']
+			occl.append(qocc)
+			occ_list.append(qocc)
+		po_dict[i] = occl
+
+	occu = list(set(occ_list))
+	for i in po_dict.keys():
+	    table.loc[table.QID == i, occu] = [1 if x in [occu.index(y) for y in po_dict[i]] else 0 for x in range(len(occu))]
+
+	return table
+
+# z = occ_mask(sparql_table(1800, 1900, "Q4263842", "Q6625963"))
+# z['text'] = z['personLabel'].apply(get_text)
+
+def cleaner(text):
+	text = str(text)
+	text = text.replace('\n','')
+	text = text.replace('&amp;',' ')
+	text = text.replace('&nbsp;','')
+	text = text.replace('#x200B;','')
+	text = text.replace('*', '')
+	text = text.replace('&gt;', '')
+	text = str(text)
+	text = text.lower()
+	return text
+
+# z['textc'] = z.text.apply(cleaner)
+
+# %%
--- sparql_to_dataframe.py
+++ sparql_to_dataframe.py
@@ -1,23 +1,26 @@
+#import os
+#os.chdir("C://brain//rutgers//564//wikiproject")
+
 from scrapes import *
+import pandas as pd
 
-# Test of imported functions
-# THESE ALL WORK! HOORAY!!!
-# Q170509 = Henry James (Example)
-
-safename("https://en.wikipedia.org/wiki/James Joyce")
-countWords("The rain in Spain stays mainly in the plain.")
-get_label("Q170509")
-countWords(get_text(get_label('Q170509')))
-
-# %%
-# Outputs dataframe based on the input range for birth dates (e.g. 1800-1900)
-# and the occupation labels, in this case literary critic AND novelist.
-
-def sparql_table(low, high):
+def sparql_table(low, high, *occs):
     dtmin = str(pd.to_datetime(low, format="%Y").date())
     dtmax = str(pd.to_datetime(high, format="%Y").date())
+    # print(occs)
+    occstr = ""
+    if len(occs) == 0:
+        return "Stop! You need to pass at least 1 (but maybe at least 2 depending on generality) occupation QIDs."
+
+    for occnum in range(len(occs)):
+        if occnum == len(occs)-1:
+            occstr += f"wd:{str(occs[occnum])}"
+        else:
+            occstr += f"wd:{str(occs[occnum])}, "
+
+    # print(occstr)
     # literary critic = Q4263842 ; novelist = Q6625963
-    query = f"SELECT DISTINCT ?personLabel ?person ?birth ?article WHERE {{?person wdt:P106 wd:Q4263842, wd:Q6625963; wdt:P569 ?birth. ?article schema:about ?person; schema:isPartOf <https://en.wikipedia.org/>; schema:inLanguage 'en'. FILTER((?birth > '{dtmin}'^^xsd:dateTime) && (?birth < '{dtmax}'^^xsd:dateTime)) SERVICE wikibase:label {{ bd:serviceParam wikibase:language '[AUTO_LANGUAGE],en'. }}}} ORDER BY (?birth)"
+    query = f"SELECT DISTINCT ?personLabel ?person ?birth ?article WHERE {{?person wdt:P106 {occstr}; wdt:P569 ?birth. ?article schema:about ?person; schema:isPartOf <https://en.wikipedia.org/>; schema:inLanguage 'en'. FILTER((?birth > '{dtmin}'^^xsd:dateTime) && (?birth < '{dtmax}'^^xsd:dateTime)) SERVICE wikibase:label {{ bd:serviceParam wikibase:language '[AUTO_LANGUAGE],en'. }}}} ORDER BY (?birth)"
     resp = S.post(url = qu, params = {'query': query, 'format': 'json'}).text
     out = json.loads(resp)
 
@@ -32,4 +35,47 @@ def sparql_table(low, high):
             data[k].append(i[k]['value'])
 
     table = pd.DataFrame(data)
+    table['personLabel'] = table['article'].apply(safename)
+    table['QID'] = list(map(lambda i : re.search("(?!\/)Q\d+", i).group(), table.person))
     return table
+
+# Test of imported functions
+
+# Q170509 = Henry James (Example)
+# safename("https://en.wikipedia.org/wiki/James Joyce")
+# countWords("The rain in Spain stays mainly in the plain.")
+# get_label("Q170509")
+# countWords(get_text(get_label('Q170509')))
+
+# %%
+
+# Outputs dataframe based on the input range for birth dates (e.g. 1800-1900) and their occupation labels,
+# which are implicitly separated by AND in the SPARQL query (e.g. critic AND novelist).
+
+# Initial surefire sparql_table function
+# def sparql_table(low, high, *occs):
+#     dtmin = str(pd.to_datetime(low, format="%Y").date())
+#     dtmax = str(pd.to_datetime(high, format="%Y").date())
+#     print(occs)
+#     print(type(occs))
+#     # literary critic = Q4263842 ; novelist = Q6625963
+#     query = f"SELECT DISTINCT ?personLabel ?person ?birth ?article WHERE {{?person wdt:P106 wd:{occs[0]}, wd:{occs[1]}; wdt:P569 ?birth. ?article schema:about ?person; schema:isPartOf <https://en.wikipedia.org/>; schema:inLanguage 'en'. FILTER((?birth > '{dtmin}'^^xsd:dateTime) && (?birth < '{dtmax}'^^xsd:dateTime)) SERVICE wikibase:label {{ bd:serviceParam wikibase:language '[AUTO_LANGUAGE],en'. }}}} ORDER BY (?birth)"
+#     resp = S.post(url = qu, params = {'query': query, 'format': 'json'}).text
+#     out = json.loads(resp)
+#
+#     data = {}
+#
+#     for i in out['results']['bindings'][0].keys():
+#         data[i] = []
+#
+#     for i in out['results']['bindings']:
+#         for j in range(len(i.keys())):
+#             k = list(i.keys())[j]
+#             data[k].append(i[k]['value'])
+#
+#     table = pd.DataFrame(data)
+#     return table
+#
+# Final entry is only 1985?
+
+# %%
